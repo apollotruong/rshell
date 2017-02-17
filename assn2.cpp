08cd2fbec48329b4dca33ec166562883e7ce81cd
Patrick Aben <paben001@ucr.edu>
2017-02-16 22:26:23 -0800

    added execute() skeleton

--- assn2.cpp
+++ assn2.cpp
@@ -66,10 +66,10 @@ public:
 List(): lines_counter(0), input(""){}
 void    read(){                         // Loads input with cin, also exits if input is exit
     cout << getenv("PWD") << " $ ";     // Prints working dir.
-    string read;                        
+    string read;
     getline(cin, read);                 // Take whatever they input on a line
     if(input == "exit"){                // Exit conditional
-            cout << "\nExiting...\n";   
+            cout << "\nExiting...\n";
             exit(0);                    // Exit
     }
     else{input = read;}                 // Sets class private to read
@@ -82,11 +82,11 @@ void    parse(){                        // Organizes input into v_lines & v_conn
     while(ss << temp){
         for(int i = 0; i < temp.length(); i++){
             if(temp.at(i) == ';'){
-                
+
                 v_temp.push_back(new Connector(temp));
             }
             if(temp.at(i) == '#'){
-                
+
             }
             if(temp.at(i) == '&'){
                 if(temp.at(i-1) == '&'){
@@ -102,9 +102,39 @@ void    parse(){                        // Organizes input into v_lines & v_conn
     }
 }
 
-void    execute();              // Execute v_lines according to v_connectors
-                                // Does logic for v_connectors
-                                // forking and execbp will be handled here
+// Execute v_lines according to v_connectors
+// Does logic for v_connectors
+// forking and execbp will be handled here
+
+void execute(){ // arguements of execute(vlines, vconnectors)
+	int i = 0; // lines iterator
+  //
+	if(v_connectors.size() == 0){
+		// if there are no connectors, jus execute the 1 and only line
+		return;
+	}
+	else{
+		// if there are connectors, run the first line, iterate to next vline
+		i++;
+	}
+	while(v_connectors.size() > 0 ){ // execute logic
+		if(v_connectors.at(0) == ";"){ // dont forget to change executed Usage to true
+			// always run the line of arguments here
+		}
+		else if(v_connectors.at(0) == "||"){
+			// run arguments here only if Usage of last line is false
+		}
+		else if(v_connectors.at(0) == "&&"){
+			// run arguements only if Usage of last line is true
+		}
+		else if(v_connectors.at(0) == "#"){ // comments
+			// never execute these lines, only ouput them
+		}
+	}
+	return;
+}
+
+
 int getInputLength(){           // Returns input.length();
     return input.length();
 }
